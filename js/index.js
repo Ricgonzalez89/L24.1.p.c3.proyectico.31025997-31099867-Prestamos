8731f2832fc72e3457e3087ea1366370dc47882c
Ricgonzalez89 <ricardoa.gonzalez04@gmail.com>
2024-11-18 16:00:44 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+/* 3. PRÉSTAMOS
+|* Se desea llevar un control de los préstamos que
+|* realiza una oficina. Se tiene por cada préstamo: nombre
+|* del cliente, código del préstamo, monto y cantidad de
+|* meses. Se requiere de un programa que permita el
+|* registro de esta información, conociendo al principio de
+|* la ejecución el monto disponible para préstamos y el
+|* porcentaje de comisión mensual que se cobrará.
+|* Estructuras de datos recomendadas
+|*  Cl_oficina: montoCaja, porcComisionMensual
+|*  Cl_prestamo: cliente, codigo, prestamo, meses
+|* Primeros requerimientos
+|*  Los datos entrada vienen en un archivo (con
+|* import... ver anexo)
+|*  Monto final disponible
+|*  Clientes que pidieron por 2 meses
+|*  Clientes que pidieron el préstamo mínimo
+*/
