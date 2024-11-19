b48e05a55ec7985d7831f39b956831a66ff2ddcf
Ricgonzalez89 <ricardoa.gonzalez04@gmail.com>
2024-11-18 23:30:42 -0400

    20241118-1130-R)-Creación-de-Clase-Mayor

--- js/Cl_oficina.js
+++ js/Cl_oficina.js
@@ -1,1 +1,76 @@
-export default class Cl_oficina {}
+export default class Cl_oficina {
+    constructor(montoDisponible, porcComisionMensual) {
+        this.montoCaja = montoDisponible;
+        this.porcComisionMensual = porcComisionMensual;
+        this.prestamos = [];
+    }
+
+    set montoCaja(mC) {
+        this._montoCaja = +mC;
+    }
+
+    set porcComisionMensual(pC) {
+        this._porcComisionMensual = +pC;
+    }
+
+    get montoCaja() {
+        return this._montoCaja;
+    }
+
+    get porcComisionMensual() {
+        return this._porcComisionMensual;
+    }
+
+    agregarPrestamo(prestamo) {
+        this.prestamos.push(prestamo);
+    }
+
+    eliminarPrestamo(codigo) { //retorna true o false si logra eliminar el prestamo
+        let indexPrestamo = -1;
+        for (let i = 0; i < this.prestamos.length; i++) {
+            if (this.prestamos[i].codigo === codigo)
+                indexPrestamo = i;
+        }
+        if (indexPrestamo !== -1) 
+            this.articulos.splice(indexPrestamo, 1);
+        return indexPrestamo !== -1;
+    }
+
+    modificarPrestamo(codigo, prestamo) { //retorna true o false si logra modificar el prestamo
+        let indexPrestamo = -1;
+        for (let i = 0; i < this.prestamos.length; i++) {
+            if (this.prestamos[i].codigo === codigo)
+                indexPrestamo = i;
+        }
+        if (indexPrestamo !== -1)
+            this.prestamos.splice(indexPrestamo, 1, prestamo);
+        return indexPrestamo !== -1;
+    }
+
+    prestamoMinimo() {
+        let menorPrestamo = this.prestamos[0].prestamo;
+        for (let i = 1; i < this.prestamos.length; i++) {
+            if (this.prestamos[i].prestamo < menorPrestamo)
+                menorPrestamo = this.prestamos[i].prestamo;
+        }
+        return menorPrestamo;
+    }
+
+    mtoFinalDisp() {
+        this.prestamos.forEach((cliente) => {
+            this.montoCaja -= cliente.prestamo;
+        })
+        return this.montoCaja;
+    }
+
+    clientesDosMeses() {
+        let resultado = this.prestamos.filter((cliente) => cliente.meses === 2);
+        return resultado;
+    }
+
+    clientesPrestamoMinimo() {
+        let menorPrestamo = this.prestamoMinimo();
+        let resultado = this.prestamos.filter((cliente) => cliente.prestamo === menorPrestamo);
+        return resultado;
+    }
+}
--- js/index.js
+++ js/index.js
@@ -16,3 +16,46 @@
 |*  Clientes que pidieron por 2 meses
 |*  Clientes que pidieron el préstamo mínimo
 */
+import Cl_prestamo from "./Cl_prestamo.js";
+import Cl_oficina from "./Cl_oficina.js";
+import Dt_prestamos from "./Dt_prestamos.js";
+import Dt_oficina from "./Dt_oficina.js";
+
+let oficina = new Cl_oficina(Dt_oficina.montoDisponible, Dt_oficina.porcComisionMensual);
+
+Dt_prestamos.forEach((persona) => {
+    oficina.agregarPrestamo(
+        new Cl_prestamo(persona.cliente, persona.codigo, persona.prestamo, persona.meses)
+    );
+})
+
+let listarPrestamos = (oficina, salida) => {
+    salida.innerHTML = "";
+    oficina.prestamos.forEach((prestamo) => {
+        salida.innerHTML += `
+            <br>Cliente: ${prestamo.cliente} - Código: ${prestamo.codigo} - 
+            Préstamo: ${prestamo.prestamo} - Meses: ${prestamo.meses}
+        `
+    });
+}
+
+let salida1 = document.getElementById("salida1");
+let salida2 = document.getElementById("salida2");
+let opciones = document.getElementById("opciones");
+
+salida1.innerHTML = `
+<br>Seleccione una opción:
+<br> 1 = Listar prestamos.
+<br> 2 = Agregar prestamos.
+<br> 3 = Eliminar prestamos.
+<br> 4 = Modificar prestamos.
+`;
+
+opciones.onclick = () => {
+    let opcion = +prompt("Ingrese su opción: ");
+    switch (opcion) {
+        case 1: 
+            listarPrestamos(oficina, salida2);
+            break;
+    }
+};
